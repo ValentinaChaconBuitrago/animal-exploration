8284fdf1e690c6f08e5a81ed15c353b66ee5c3b1
ValentinaChaconBuitrago <v.chacon@uniandes.edu.co>
2020-03-08 00:13:17 -0500

    H

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,24 +1,29 @@
 const targetDB = document.querySelector("#chose-database");
 
-const renderCollections = collections =>{
+const renderCollections = collections => {
   console.log("got collections", collections);
   let selectedCollection = document.querySelector("#chose-collection");
-  selectedCollection.innerHTML ="";
-  for(const collec of collections){
+  selectedCollection.innerHTML = "";
+  for (const collec of collections) {
     const option = document.createElement("option");
-    option.setAttribute("value",collec.name);
+    option.setAttribute("value", collec.name);
     option.textContent = collec.name;
     selectedCollection.append(option);
-  };
+  }
 };
 
+function onDBSelection() {
+  //Original component
+  const dateDiv = document.getElementById("show-dbs");
+  dateDiv.innerHTML = Date();
 
-function onDBSelection(){
+
+
   let dbName = targetDB.value;
   console.log("Data Base selected", dbName);
   let selectedCollection = document.querySelector("#chose-collection");
 
-  if(dbName !== "no data"){
+  if (dbName !== "no data") {
     fetch(`./database/${dbName}`)
       .then(res => res.json())
       .then(renderCollections)
@@ -28,10 +33,11 @@ function onDBSelection(){
         div.textContent = "Error downloading data";
         document.getElementById("coll").append(div);
       });
+  } else {
+    selectedCollection.innerHTML = "Select a type of animal";
   }
-  else{
-    selectedCollection.innerHTML ="Select a type of animal";
-  }
-};
+}
 
 targetDB.addEventListener("change", onDBSelection);
+
+
